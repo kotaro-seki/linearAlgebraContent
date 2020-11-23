1087d7bcb885b4e125c83853a28ba3db05465dd4
関倖太郎 <kotaro.seki1216@gmail.com>
2020-11-24 07:17:50 +0900

    #3 determinantで行列のバリデーションを追加 * フォーマット忘れてた * 渡された行列がN次正方行列になっていなかったらエラーを返すよう変更 * エラーメッセージを受け取って定形のエラーレスポンスを返すメソッドを追加

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-from typing import List, Optional
+from typing import List
 
-from fastapi import FastAPI
-from pydantic import BaseModel
 import numpy as np
 import numpy.linalg as LA
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+
+@app.get('/')
 def root():
-    return {"path": "root"}
+    return {'path': 'root'}
+
 
 class Matrix(BaseModel):
     n: int
     matrix: List[List[int]]
 
 
-@app.post("/determinant/")
+@app.post('/determinant/')
 def determinant(matrix: Matrix):
+    if not is_n_dimension_matrix(matrix):
+        return make_error_response('渡された行列がN次の正方行列ではありませんでした。')
     determinant = LA.det(np.array(matrix.matrix))
-    return {"n": matrix.n, "matrix": matrix.matrix, "determinant": determinant}
+    return {'n': matrix.n, 'matrix': matrix.matrix, 'determinant': determinant}
+
+
+def is_n_dimension_matrix(matrix: Matrix) -> bool:
+    if len(matrix.matrix) != matrix.n:
+        return False
+    for row in matrix.matrix:
+        if len(row) != matrix.n:
+            return False
+    return True
+
+
+def make_error_response(error_message: str) -> dict:
+    return {
+        'status': 'error',
+        'error_message': error_message
+    }
